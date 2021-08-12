ac055bf4b509c46db4cce03a1f8f94cd2575594c
snaipp1 <snaipp1@yandex.by>
2021-08-12 13:21:12 +0300

    feat: add Counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Counter from './components/Counter';
 
 
 function App() {
+
   return (
     <div className="App">
-      
+      <Counter/>
     </div>
   );
 }
