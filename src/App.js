9ea6e8cc27ce5abba9d10c38868f2eba83efd50f
snaipp1 <snaipp1@yandex.by>
2021-09-01 11:20:14 +0300

    feat: add ui components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 import './styles/App.css';
 
 
@@ -11,8 +13,34 @@ function App() {
     {id: 3, title: 'TypeScript', body: 'Description'},
   ]);
 
+  const [post, setPost] = useState({title: '', body: ''});
+
+  
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    
+    setPosts([...posts, {...post, id: Date.now()}]);
+    setPost({title: '', body: ''});
+  };
+
   return (
     <div className="App">
+      <form>
+        <MyInput 
+          type="text" 
+          placeholder="Название поста"
+          value={post.title}
+          onChange={e => setPost({...post, title: e.target.value})}
+        /> 
+        <MyInput
+          value={post.body} 
+          onChange={e => setPost({...post, body: e.target.value})}
+          type="text" 
+          placeholder="Описание поста"
+        />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton> 
+      </form>
       <PostList posts={posts} title="Список постов"/>
     </div>
   );
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const PostItem = (props) => {
 
-  const {id, title, body} = props.post;
+  const {title, body} = props.post;
 
     return (
     <div className="post">
         <div className="post__content">
-          <strong>{id}. {title}</strong>
+          <strong>{props.number}. {title}</strong>
           <div>
             {body}
           </div>
