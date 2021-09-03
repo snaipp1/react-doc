52862abbc68312943ff04e203b6955bf718083cb
snaipp1 <snaipp1@yandex.by>
2021-09-03 12:40:45 +0300

    feat: add search in ui components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useMemo} from 'react';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
+import MyInput from './components/UI/input/MyInput';
 import MySelect from './components/UI/select/MySelect';
 import './styles/App.css';
 
@@ -12,14 +13,27 @@ function App() {
     {id: 2, title: 'Java', body: 'Description text'},
     {id: 3, title: 'TypeScript', body: 'Description message'},
   ]);
-
+  const [searchQuery, setSearchQuery] = useState('');
   const [selectedSort, setSelectedSort] = useState('');
+
+  const sortedPosts = useMemo(() => {
+    console.log('Функция перерисовала')
+    if (selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+    } else {
+      return posts;
+    }
+  }, [selectedSort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()));
+  }, [searchQuery, sortedPosts]);
+
   const createPost = (newPost) => setPosts([...posts, newPost]);
   const removePost = (post) => setPosts(posts.filter(p => p.id !== post.id));
 
   const sortPosts = (sort) =>{
     setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
 
   return (
@@ -27,6 +41,11 @@ function App() {
       <PostForm create={createPost}/>
       <hr style={{margin: '15px 0'}}/>
       <div>
+        <MyInput
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)} 
+          placeholder="Поиск..."
+        />
         <MySelect
           value={selectedSort}
           onChange={sortPosts}
@@ -38,7 +57,7 @@ function App() {
         />
       </div>
       {posts.length !== 0
-        ? <PostList remove={removePost} posts={posts} title="Список постов"/>
+        ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов"/>
         : <h1 style={{textAlign: 'center', marginTop: "20px"}}>
             Постов пока нет...
           </h1>
